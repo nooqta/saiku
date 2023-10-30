daaa83a31e54f8053e708c1041a2a8f2c259f0af
Anis Marrouchi <anismarrouchi@hotmail.com>
2023-10-30 09:39:31 +0100

    update chrome extension

--- extensions/chrome/js/openai_content.js
+++ extensions/chrome/js/openai_content.js
@@ -1,3 +1,68 @@
+const OriginalXMLHttpRequest = window.XMLHttpRequest;
+
+function PatchedXMLHttpRequest() {
+  const xhrInstance = new OriginalXMLHttpRequest();
+
+  const originalOpen = xhrInstance.open;
+  const originalSend = xhrInstance.send;
+
+  // Override the `open` method to capture method and URL
+  xhrInstance.open = function (method, url, ...rest) {
+    this._url = url; // Store the URL for later use
+    this._method = method; // Store the method for later use
+    return originalOpen.apply(this, [method, url, ...rest]);
+  };
+
+  // Override the `send` method
+  xhrInstance.send = function (...args) {
+    console.log("Intercepted XHR request:", this._method, this._url);
+    if (
+      this._url.includes("https://chat.openai.com/backend-api/conversation") &&
+      this._method === "POST"
+    ) {
+      // You can add additional logic here
+
+      // For example, attaching an event listener to handle the load event:
+      this.addEventListener("load", function () {
+        const contentType = this.getResponseHeader("content-type");
+        if (contentType && contentType.includes("text/event-stream")) {
+          // Handle the SSE stream
+          handleSSEStream(this.response);
+        }
+      });
+    }
+
+    return originalSend.apply(this, args);
+  };
+
+  return xhrInstance;
+}
+
+window.XMLHttpRequest = PatchedXMLHttpRequest;
+
+
+// Handle the streamed response
+function handleSSEStream(stream) {
+  const reader = stream.getReader();
+
+  reader.read().then(function processText({ done, value }) {
+    if (done) {
+      console.log("Stream complete.");
+      return;
+    }
+
+    // Convert the Uint8Array to a string
+    const textChunk = new TextDecoder().decode(value);
+    console.log("Received Chunk:", textChunk);
+
+    // Process the chunk as needed
+    processReceivedData(textChunk);
+
+    // Read and process the next chunk
+    return reader.read().then(processText);
+  });
+}
+
 // Function to set the user's input in the textarea and click the send button
 function setAndSend(userInput) {
   const textarea = document.getElementById("prompt-textarea");
@@ -13,45 +78,21 @@ function setAndSend(userInput) {
   document.querySelector('[data-testid="send-button"]').click();
 }
 
-// Function to observe changes in the chat area for new messages
-function observeChatForNewMessage(callback) {
-    const chatArea = document.querySelector('div[data-testid^="conversation-turn-"]').parentNode;
-    if (!chatArea) return;
-
-    const observer = new MutationObserver(mutationsList => {
-        for (let mutation of mutationsList) {
-            if (mutation.type === "childList" && mutation.addedNodes.length) {
-                const addedNode = Array.from(mutation.addedNodes).pop(); // Get the last added node
-                if (addedNode && addedNode.nodeType === Node.ELEMENT_NODE) {
-                    const assistantMessage = addedNode.querySelector('[data-message-author-role="assistant"]');
-                    console.log("New message from observe:", assistantMessage);
-                    if (assistantMessage && assistantMessage.innerText) {
-                        callback(assistantMessage.innerText); // Pass the response text to the callback
-                    }
-                }
-            }
-        }
-    });
-
-    observer.observe(chatArea, { childList: true });
+function processReceivedData(data) {
+  // Process the received SSE data
+  console.log("Processed data:", data);
+  chrome.runtime.sendMessage({
+    action: "receivedResponse",
+    responseText: data,
+  });
 }
 
-
-
-// Listen for messages from the background script or popup script
+// Listen for messages from the background script
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "setAndSend") {
     setAndSend(request.userInput);
-
-    // Observe chat for a new message (response from OpenAI)
-    observeChatForNewMessage((responseText) => {
-      // Once you get the response, you can send it back to the background script or do other tasks
-      chrome.runtime.sendMessage({
-        action: "receivedResponse",
-        responseText: responseText,
-      });
-    });
-
     sendResponse({ status: "Message set and button clicked" });
   }
 });
+
+console.log("OpenAI content script loaded");
