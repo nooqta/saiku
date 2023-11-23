31de90e06b1721dcdb4fd6f1f5b1cb084d3a2dac
Anis Marrouchi <anismarrouchi@hotmail.com>
2023-11-23 14:34:07 +0100

    Update GitAction class

--- src/actions/git.ts
+++ src/actions/git.ts
@@ -10,54 +10,27 @@ export default class GitAction implements Action {
   dependencies = []; // Dependencies for git, normally git would be pre-installed
   agent: Agent;
   name = 'git_action';
-  description = 'Perform common Git operations';
-  parameters =[
-    { name: 'operation', type: 'string', required: true, enum: ['clone', 'add', 'commit', 'push', 'pull', 'init'], description: 'Git operation to perform' },
-    { name: 'repository', type: 'string', required: false },
-    { name: 'path', type: 'string', required: false },
-    { name: 'message', type: 'string', required: false, description: 'Commit message. Required for commit operation.' }
+  description = 'Execute a Git command';
+  parameters = [
+    { name: 'command', type: 'string', required: true, description: 'The git command to execute' },
+    { name: 'path', type: 'string', required: false, description: 'The path to the repository' }
   ];
-  
+
   constructor(agent: Agent) {
     this.agent = agent;
   }
 
-  async run(args: { operation: string, repository?: string, path?: string, message?: string }): Promise<any> {
-    let cmd;
+  async run(args: { command: string, path?: string }): Promise<any> {
     const workingDir = args.path ? path.resolve(args.path) : process.cwd();
+    const cmd = `git -C "${workingDir}" ${args.command}`;
     
-    switch (args.operation) {
-      case 'clone':
-        if (!args.repository) throw new Error('Repository URL is required for clone operation');
-        cmd = `git clone ${args.repository} "${workingDir}"`;
-        break;
-      case 'add':
-        cmd = `git -C "${workingDir}" add .`;
-        break;
-      case 'commit':
-        if (!args.message) throw new Error('Commit message is required for commit operation');
-        cmd = `git -C "${workingDir}" commit -m "${args.message}"`;
-        break;
-      case 'push':
-        cmd = `git -C "${workingDir}" push`;
-        break;
-      case 'pull':
-        cmd = `git -C "${workingDir}" pull`;
-        break;
-      case 'init':
-        cmd = `git -C "${workingDir}" init`;
-        break;
-      default:
-        throw new Error(`Invalid Git operation: ${args.operation}`);
-    }
-
     try {
       const { stdout, stderr } = await execAsync(cmd);
       if (stderr) throw new Error(stderr);
       console.log(stdout);
       return stdout;
     } catch (error) {
-        // @ts-ignore
+      // @ts-ignore
       console.error(`Error executing Git command: ${error.message}`);
       throw error;
     }
